d76ca7a704dabd7fc00b2733e9b90f16c2fbd4fa
Dinindu Senanayake <dinindusen@gmail.com>
2026-06-04 19:59:30 +0100

    add comments

--- nichecompass/INSTALL_NICHECOMPASS_GH200.sh
+++ nichecompass/INSTALL_NICHECOMPASS_GH200.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 module purge
 
 export PATH="/apps/kir/eb/hpc-utils/aarch64:$PATH"
@@ -6,6 +8,8 @@ export CUDA_HOME=/usr/local/cuda
 export PYTHONNOUSERSITE=1
 module use /apps/eb/el9/2025a/aarch64/modules/all/
 
+# Set these env variables to a shared path
+# Otherwise, uv will cache python binary to home directory .i.e. can not share from home
 export UV_PYTHON_INSTALL_DIR=""
 export UV_CACHE_DIR=""
 
@@ -15,14 +19,17 @@ uv pip install torch_scatter torch_sparse \
     --no-build-isolation \
     -f https://data.pyg.org/whl/torch-2.6.0+cu126.html
 
+# pyg-lib for aarch64 has to be compiled from source as there
+# aren't any pre-compiled binaries
 cd pyg-lib
 git clone https://github.com/pyg-team/pyg-lib.git
 git submodule update --init --recursive
 
 TORCH_CUDA_ARCH_LIST="9.0" \
-MAX_JOBS=4 \
-uv pip install . --no-build-isolation -v
+    MAX_JOBS=4 \
+    uv pip install . --no-build-isolation -v
 
+# few dependencies require fortran compiler. Provide it via GCC
 module load GCC/14.2.0
-uv pip install jax[cuda12] nichecompass[all] 
+uv pip install jax[cuda12] nichecompass[all]
 uv pip install squidpy gdown
